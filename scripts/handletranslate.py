17f9ed1f9bfb7ba93e498d180f147f552b718cbb
legolas-zeng <814382690@qq.com>
2019-08-21 23:29:55 +0800

    更新判断多个翻译接口

--- scripts/handletranslate.py
+++ scripts/handletranslate.py
@@ -2,9 +2,10 @@
 # @Time    : 2019/8/13 13:56
 # @Author  : zwa
 from newsweb.models import *
-from scripts import translater,translater2,functions
+from scripts import translater,translater2,functions,translation
 # from GoogleFreeTrans import Translator
 import re
+
 
 
 def handledata(id,article:str):
@@ -48,4 +49,12 @@ def cut_text(text,lenth=4000) -> list:
 	textArr = re.findall('.{' + str(lenth) + '}', retext)
 	textArr.append(text[(len(textArr) * lenth):])
 	return textArr
-	
+
+def HandleData(id,article):
+	data = functions.QueryGetOneAriticle(article, id)
+	trans1 = translation.Translation(data[0].get('content'))
+	_bool1, content1 = trans1.goole_translation_free()
+	trans2 = translation.Translation(data[0].get('title'))
+	_bool2, content2 = trans2.goole_translation_free()
+	if _bool1 and _bool2:
+		return content1, content2
